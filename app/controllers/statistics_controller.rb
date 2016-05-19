aa8610f6e0e4f28f769d4183399c7b03bd324f54
schwarzritter09 <le.cadeau.de.un.ange0911@gmail.com>
2016-05-19 14:47:14 +0900

    modify statistics page

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -9,6 +9,7 @@ class StatisticsController < ApplicationController
     @tagedCount = taged_ids.count
     @notTagedCount = nottaged_ids.count
     @rankOfEntryCount = Tag.order("count_member_id desc").group(:member_id).count(:member_id)
-    
+    solo_tag_ids = Tag.group(:photo_id).having("count(photo_id) = 1").pluck(:id)
+    @rankOfSoloEntryCount = Tag.where(id: solo_tag_ids).order("count_member_id desc").group(:member_id).count(:member_id)
   end
 end
