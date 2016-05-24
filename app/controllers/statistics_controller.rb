660c287e7329b68772378263ad05eb806711d730
schwarzritter09 <le.cadeau.de.un.ange0911@gmail.com>
2016-05-24 13:40:04 +0900

    modify statistics page

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -8,40 +8,43 @@ class StatisticsController < ApplicationController
     nottaged_ids = Photo.where.not(id: taged_ids)
     @tagedCount = taged_ids.count
     @notTagedCount = nottaged_ids.count
-    
-    # 写真全体での統計
-    entryCount = Tag.order("count_member_id desc").group(:member_id).count(:member_id)
-    solo_photo_ids = Tag.select(:photo_id).group(:photo_id).having("count(photo_id) = 1").pluck(:photo_id)
-    soloEntryCount = Tag.where(photo_id: solo_photo_ids).order("count_member_id desc").group(:member_id).count(:member_id)
-        
+            
     @members = Member.where.not(id: [27,28])
     @ranks = Hash.new
     @members.each do |m|
-      entryCount[m.id] = 0 if entryCount[m.id].nil?
-      soloEntryCount[m.id] = 0 if soloEntryCount[m.id].nil?
-
+      
+      # 写真全体での統計
+      ## 自身が写っている画像をカウント
+      entry = Photo.joins(:tags).where("tags.member_id = ?", m.id).uniq(:id)
+      ## 他人が写っている画像をカウント
+      otherEntry = Photo.joins(:tags).where(id: entry.pluck(:id)).where.not("tags.member_id = ?", m.id).uniq(:id)
+      ## 自身のみ写っている画像のカウント(自身が載っている画像のカウントから、他人が写っているカウントを除外)
+      soloEntryCount = entry.count - otherEntry.count
+      
       # 自身の上げたブログでの統計
-      entryCountByOwner = Article.where(member_id: m.id).joins(:photos).count()
-      solo_photo_by_owner_ids = Tag.where(member_id: m.id).joins(:article).select(:photo_id).where("articles.member_id = ?", m.id).group(:photo_id).having("count(photo_id) = 1"
-).pluck(:photo_id)
-      soloEntryCountByOwner = Tag.where(member_id: m.id).where(photo_id: solo_photo_by_owner_ids).order("count_member_id desc").group(:member_id).count(:member_id)
-
-      if entryCount[m.id].present? && soloEntryCount[m.id].present? && entryCount[m.id] != 0 && soloEntryCount[m.id] != 0
-        ratio = soloEntryCount[m.id]/entryCount[m.id].to_f*100
+      ## 自身が上げた画像のカウント
+      entryByOwner = Photo.joins(:tags, :article).where("tags.member_id = ?", m.id).where("articles.member_id = ?", m.id).uniq(:id)
+      ## 自身が上げた画像の中で、他人が写っている画像のカウント
+      otherEntryByOwner = Photo.joins(:tags, :article).where.not("tags.member_id = ?", m.id).where("articles.member_id = ?", m.id).uniq(:id)
+      ## 自身のみ写っている画像のカウント(自身が上げた画像のカウントから、他人が写っているカウントを除外)
+      soloEntryByOwnerCount = entryByOwner.count - otherEntryByOwner.count
+      
+      if entry.count != 0 && soloEntryCount != 0
+        ratio = soloEntryCount/entry.count.to_f*100
         ratio = ratio.round(3)
       else
         ratio = 0
       end
       
-      if entryCountByOwner.present? && soloEntryCountByOwner[m.id].present? && entryCountByOwner != 0 && soloEntryCountByOwner[m.id] != 0
-        ratioByOwner = soloEntryCountByOwner[m.id]/entryCountByOwner.to_f*100
+      if entryByOwner.count != 0 && soloEntryByOwnerCount != 0
+        ratioByOwner = soloEntryByOwnerCount/entryByOwner.count.to_f*100
         ratioByOwner = ratioByOwner.round(3)
       else
         ratioByOwner = 0
       end
       
-      @ranks[m.id] = {:count=>entryCount[m.id], :soloCount=>soloEntryCount[m.id], :ratio=>ratio,
-                      :countByOwner=>entryCountByOwner, :soloCountByOwner=>soloEntryCountByOwner[m.id], :ratioByOwner=>ratioByOwner}
+      @ranks[m.id] = {:count=>entry.count, :soloCount=>soloEntryCount, :ratio=>ratio,
+                      :countByOwner=>entryByOwner.count, :soloCountByOwner=>soloEntryByOwnerCount, :ratioByOwner=>ratioByOwner}
     end
   end
 end
