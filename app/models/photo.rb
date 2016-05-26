ef28b600949db3168af18321f8c4e64ea3d0d9b4
schwarzritter09 <le.cadeau.de.un.ange0911@gmail.com>
2016-05-27 01:49:24 +0900

    modify only+and search

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -20,7 +20,7 @@ class Photo < ActiveRecord::Base
       else
         if photo_search[:only].present? && photo_search[:member_id].size == 1
           
-          # 一旦member_idが一致するphoto_idを検索
+          # 単独メンバーのONLY検索(1人しかいないのでANDは無視できる)
           target_photo_ids = Tag.select(:photo_id).
                              where("count > 0").
                              where(photo_id: Tag.where("count > 0").where(member_id: photo_search[:member_id]).pluck(:photo_id)).
@@ -31,15 +31,40 @@ class Photo < ActiveRecord::Base
           
         elsif photo_search[:and].present? || photo_search[:only].present?
           
-          target_photo_ids = Tag.select(:photo_id).where("count > ?", 0).where(member_id: photo_search[:member_id]).group(:photo_id)
-             
           if photo_search[:only].present?
-            target_photo_ids = target_photo_ids.having("count(photo_id) = ?", photo_search[:member_id].size)
-          else
-            target_photo_ids = target_photo_ids.having("count(photo_id) >= ?", photo_search[:member_id].size)
-          end
+            # 複数メンバーのAND, ONLY検索
+            
+            # まずは指定メンバー数のみのタグを持つPhotoのidを検索
+            # この時点ではメンバーを絞っていない
+            target_photo_ids = Tag.select(:photo_id).
+                                   where("count > ?", 0).
+                                   group(:photo_id).
+                                   having("count(photo_id) = ?", photo_search[:member_id].size)
           
-          photos = photos.where(id: target_photo_ids.uniq.pluck(:photo_id))
+            # 一旦抽出
+            photos = photos.where(id: target_photo_ids.uniq.pluck(:photo_id))
+            
+            # 指定メンバーのタグを持つPhotoを抽出
+            photo_search[:member_id].each do |mid|
+              photos = photos.where(id: Tag.where(member_id: mid).pluck(:photo_id))
+            end
+          else
+            # 複数メンバーのAND, IN検索
+            # まずは指定メンバー数以上のタグを持つPhotoのidを検索
+            # この時点ではメンバーを絞っていない
+            target_photo_ids = Tag.select(:photo_id).
+                                   where("count > ?", 0).
+                                   group(:photo_id).
+                                   having("count(photo_id) >= ?", photo_search[:member_id].size)
+          
+            # 一旦抽出
+            photos = photos.where(id: target_photo_ids.uniq.pluck(:photo_id))
+            
+            # 指定メンバーのタグを持つPhotoを抽出
+            photo_search[:member_id].each do |mid|
+              photos = photos.where(id: Tag.where(member_id: mid).pluck(:photo_id))
+            end
+          end
         else
           photos = photos.has_tag_member_id(photo_search[:member_id]) if photo_search[:member_id].present?
         end
